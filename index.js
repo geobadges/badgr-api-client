6323f36e7a066826cca6ce5c755a21d59cd59b72
DanielJDufour <daniel.j.dufour@gmail.com>
2020-06-14 14:47:57 -0400

    updated

--- index.js
+++ index.js
@@ -1,110 +1,112 @@
-const axios = require('axios');
-const FormData = require('form-data');
-const pick = require('lodash.pick');
+const axios = require('axios')
+const FormData = require('form-data')
+const assign = require('lodash.assign')
+const pick = require('lodash.pick')
 
-const prefix = '[badgr-api-client]';
+const prefix = '[badgr-api-client]'
 
 class Client {
-
-  constructor({ debug = false, endpoint, username, password } = {}) {
-    this.debug = debug;
-    this.endpoint = endpoint;
-    this.username = username;
-    this.password = password;
-    if (this.debug) console.log(`${prefix} constructed Badgr API Client`);
+  constructor ({ debug = false, endpoint, username, password } = {}) {
+    this.debug = debug
+    this.endpoint = endpoint
+    this.username = username
+    this.password = password
+    if (this.debug) console.log(`${prefix} constructed Badgr API Client`)
   }
 
-  log() {
-    if (this.debug) console.log(`${prefix} this.endpoint: ${this.endpoint}`);
-    if (this.debug) console.log(`${prefix} this.password: ${this.password}`);
-    if (this.debug) console.log(`${prefix} this.username: ${this.username}`);
+  log () {
+    if (this.debug) console.log(`${prefix} this.endpoint: ${this.endpoint}`)
+    if (this.debug) console.log(`${prefix} this.password: ${this.password}`)
+    if (this.debug) console.log(`${prefix} this.username: ${this.username}`)
   }
 
-  async initialize() {
+  async initialize () {
     const {
       accessToken,
       expiresIn,
       expirationDate,
       refreshToken
-    } = await this.getAccessToken();
-    this.accessToken = accessToken;
-    this.expiresIn = expiresIn;
-    this.expirationDate = expirationDate;
-    this.refreshToken = refreshToken;
-    if (this.debug) console.log(`${prefix} initialized`);
+    } = await this.getAccessToken()
+    this.accessToken = accessToken
+    this.expiresIn = expiresIn
+    this.expirationDate = expirationDate
+    this.refreshToken = refreshToken
+    if (this.debug) console.log(`${prefix} initialized`)
   }
 
-  async getAccessToken({ endpoint, username, password } = {}) {
+  async getAccessToken ({ endpoint, username, password } = {}) {
     try {
-      if (this.debug) console.log(`${prefix} starting getAccessToken with`, { endpoint, username, password });
-      endpoint = endpoint || this.endpoint;
-      if (this.debug) console.log(`${prefix} endpoint: ${endpoint}`);
-      password = password || this.password;
-      if (this.debug) console.log(`${prefix} password: ${password}`);
-      username = username || this.username;
-      if (this.debug) console.log(`${prefix} username: ${username}`);
+      if (this.debug) console.log(`${prefix} starting getAccessToken with`, { endpoint, username, password })
+      endpoint = endpoint || this.endpoint
+      if (this.debug) console.log(`${prefix} endpoint: ${endpoint}`)
+      password = password || this.password
+      if (this.debug) console.log(`${prefix} password: ${password}`)
+      username = username || this.username
+      if (this.debug) console.log(`${prefix} username: ${username}`)
 
-      const url = endpoint + '/o/token';
+      const url = endpoint + '/o/token'
 
-      const formData = new FormData();
-      formData.append("grant_type", "password");
-      formData.append("client_id", "public");
-      formData.append("scope", "rw:backpack rw:issuer rw:profile");
-      formData.append("username", username);
-      formData.append("password", password);
+      const formData = new FormData()
+      formData.append('grant_type', 'password')
+      formData.append('client_id', 'public')
+      formData.append('scope', 'rw:backpack rw:issuer rw:profile')
+      formData.append('username', username)
+      formData.append('password', password)
 
-      const headers = {
-        ...formData.getHeaders(),
-        "Content-Length": formData.getLengthSync()
-      };
+      const headers = {}
+      if (formData.getHeaders) {
+        assign(headers, formData.getHeaders())
+      }
+      if (formData.getLengthSync) {
+        headers['Content-Length'] = formData.getLengthSync()
+      }
 
       const response = await axios
-      .create({ headers })
-      .post(url, formData);
+        .create({ headers })
+        .post(url, formData)
 
-      const { data } = response;
+      const { data } = response
 
-      const accessToken = data.access_token;
-      const refreshToken = data.refresh_token;
-      const expiresIn = data.expires_in;
-      const currentDate = new Date();
-      const expirationDate = new Date(currentDate.getTime() + expiresIn * 60 * 1000);  
-      return { accessToken, refreshToken, expiresIn, expirationDate };
-
+      const accessToken = data.access_token
+      const refreshToken = data.refresh_token
+      const expiresIn = data.expires_in
+      const currentDate = new Date()
+      const expirationDate = new Date(currentDate.getTime() + expiresIn * 60 * 1000)
+      return { accessToken, refreshToken, expiresIn, expirationDate }
     } catch (error) {
-      console.error("error:", error);
-      throw new Error("failed to get access token");
+      console.error('error:', error)
+      throw new Error('failed to get access token')
     }
   }
 
-  async getBadgeAssertions({ accessToken = this.accessToken, endpoint=this.endpoint, entityId, fields=['entityId'] }) {
+  async getBadgeAssertions ({ accessToken = this.accessToken, endpoint = this.endpoint, entityId, fields = ['entityId'] }) {
     const response = await axios({
       params: { access_token: accessToken },
       method: 'GET',
       url: `${endpoint}/v2/badgeclasses/${entityId}/assertions`
-    });
-    return response.data.result.map(assertion => pick(assertion, fields));
+    })
+    return response.data.result.map(assertion => pick(assertion, fields))
   }
 
-  async getBadgeClasses({ accessToken = this.accessToken, endpoint=this.endpoint, fields=['entityId'] }) {
-      const response = await axios({
-          params: { access_token: accessToken },
-          method: 'GET',
-          url: `${endpoint}/v2/badgeclasses`
-      });
-      return response.data.result.map(badgeClass => pick(badgeClass, fields));
+  async getBadgeClasses ({ accessToken = this.accessToken, endpoint = this.endpoint, fields = ['entityId'] }) {
+    const response = await axios({
+      params: { access_token: accessToken },
+      method: 'GET',
+      url: `${endpoint}/v2/badgeclasses`
+    })
+    return response.data.result.map(badgeClass => pick(badgeClass, fields))
   }
 
-  async getIssuer({ accessToken = this.accessToken, endpoint=this.endpoint, entityId, fields=['entityId', 'name'] }) {
-    if (!entityId) throw new Error('You must supply an entityId');
-    if (!accessToken) throw new Error('Access Token must be set');
+  async getIssuer ({ accessToken = this.accessToken, endpoint = this.endpoint, entityId, fields = ['entityId', 'name'] }) {
+    if (!entityId) throw new Error('You must supply an entityId')
+    if (!accessToken) throw new Error('Access Token must be set')
     const response = await axios({
       params: { access_token: accessToken },
       method: 'GET',
       url: `${endpoint}/v2/issuers/${entityId}`
-    });
-    return pick(response.data.result[0], fields);
+    })
+    return pick(response.data.result[0], fields)
   }
 }
 
-module.exports = Client;
+module.exports = Client
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const test = require('ava');
-const API = require('./index.js');
+const test = require('ava')
+const API = require('./index.js')
 
 const {
   BADGR_API_CLIENT_ENDPOINT,
@@ -7,33 +7,30 @@ const {
   BADGR_API_CLIENT_USER,
   BADGR_API_ISSUER_ENTITY_ID,
   BADGR_API_BADGE_CLASS_ENTITY_ID
-} = process.env;
+} = process.env
 
 test('initialization', async t => {
-
   const api = new API({
     debug: false,
     endpoint: BADGR_API_CLIENT_ENDPOINT,
     password: BADGR_API_CLIENT_PASSWORD,
     username: BADGR_API_CLIENT_USER
-  });
+  })
 
-  await api.initialize();
+  await api.initialize()
 
-  const accessToken = api.accessToken;
-  const refreshToken = api.refreshToken;
+  const accessToken = api.accessToken
+  const refreshToken = api.refreshToken
 
-  t.true(accessToken.length > 10);
-  t.true(refreshToken.length > 10);
-
-});
+  t.true(accessToken.length > 10)
+  t.true(refreshToken.length > 10)
+})
 
 test('getting accessToken', async t => {
-
   const api = new API({
     debug: false,
     endpoint: BADGR_API_CLIENT_ENDPOINT
-  });
+  })
 
   const {
     accessToken,
@@ -43,13 +40,13 @@ test('getting accessToken', async t => {
   } = await api.getAccessToken({
     password: BADGR_API_CLIENT_PASSWORD,
     username: BADGR_API_CLIENT_USER
-  });
+  })
 
-  t.true(accessToken.length > 10);
-  t.true(refreshToken.length > 10);
-  t.true(expirationDate instanceof Date);
-  t.is(typeof expiresIn, 'number');
-});
+  t.true(accessToken.length > 10)
+  t.true(refreshToken.length > 10)
+  t.true(expirationDate instanceof Date)
+  t.is(typeof expiresIn, 'number')
+})
 
 test('getting all the badge classes', async t => {
   const api = new API({
@@ -57,21 +54,21 @@ test('getting all the badge classes', async t => {
     endpoint: BADGR_API_CLIENT_ENDPOINT,
     password: BADGR_API_CLIENT_PASSWORD,
     username: BADGR_API_CLIENT_USER
-  });
+  })
 
-  await api.initialize();
+  await api.initialize()
 
   const badges = await api.getBadgeClasses({
     fields: ['entityId', 'name']
-  });
+  })
 
-  t.true(badges.length >= 5);
-  t.true(badges[0].hasOwnProperty('entityId'));
-  t.true(badges[0].hasOwnProperty('name'));
+  t.true(badges.length >= 5)
+  t.true(badges[0].hasOwnProperty('entityId'))
+  t.true(badges[0].hasOwnProperty('name'))
 
   // don't return createdBy unless explicitly ask for it
-  t.false(badges[0].hasOwnProperty('createdBy'));
-});
+  t.false(badges[0].hasOwnProperty('createdBy'))
+})
 
 test('getting all the badge assertions', async t => {
   const api = new API({
@@ -79,47 +76,44 @@ test('getting all the badge assertions', async t => {
     endpoint: BADGR_API_CLIENT_ENDPOINT,
     password: BADGR_API_CLIENT_PASSWORD,
     username: BADGR_API_CLIENT_USER
-  });
+  })
 
-  await api.initialize();
+  await api.initialize()
 
   const assertions = await api.getBadgeAssertions({
     entityId: BADGR_API_BADGE_CLASS_ENTITY_ID,
     fields: ['entityId', 'name']
-  });
+  })
 
-  t.true(assertions.length >= 1);
-  t.true(assertions[0].hasOwnProperty('entityId'));
+  t.true(assertions.length >= 1)
+  t.true(assertions[0].hasOwnProperty('entityId'))
 
   // don't return recipient unless explicitly ask for it
-  t.false(assertions[0].hasOwnProperty('recipient'));
-});
+  t.false(assertions[0].hasOwnProperty('recipient'))
+})
 
 test('getting issuer by entity id', async t => {
-
   const api = new API({
     debug: false,
     endpoint: BADGR_API_CLIENT_ENDPOINT,
     password: BADGR_API_CLIENT_PASSWORD,
     username: BADGR_API_CLIENT_USER
-  });
+  })
 
-  await api.initialize();
+  await api.initialize()
 
   const issuer = await api.getIssuer({
     entityId: BADGR_API_ISSUER_ENTITY_ID,
     fields: ['name', 'entityId', 'description']
-  });
+  })
 
-  t.true(issuer.hasOwnProperty('name'));
-  t.true(issuer.name.length >= 3);
-  t.true(issuer.hasOwnProperty('entityId'));
-  t.true(issuer.entityId.length >= 10);
-  t.true(issuer.hasOwnProperty('description'));
-  t.true(issuer.description.length > 10);
+  t.true(issuer.hasOwnProperty('name'))
+  t.true(issuer.name.length >= 3)
+  t.true(issuer.hasOwnProperty('entityId'))
+  t.true(issuer.entityId.length >= 10)
+  t.true(issuer.hasOwnProperty('description'))
+  t.true(issuer.description.length > 10)
 
   // make sure staff isn't included in the return
-  t.false(issuer.hasOwnProperty('staff'));
-});
-
-
+  t.false(issuer.hasOwnProperty('staff'))
+})
